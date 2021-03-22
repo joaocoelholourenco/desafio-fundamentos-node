a854351511ec4e00d700d21e44f176b1ea237447
João Vitor <joaovitorcoelho13@gmail.com>
2021-03-22 20:58:47 -0300

    finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import uiid, { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome, total } = this.transactions.reduce(
+      (accumalator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumalator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumalator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumalator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const resTotal = income - outcome;
+
+    return { income, outcome, total: resTotal };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
